7a04da7b3ee82be2513e80fabfcdfb0aa66355bb
Suchindra Sandhu <suchindra@gmail.com>
2012-10-29 17:38:21 -0400

    Merged safeToH5 into toH5. Use --safe to invoke safe behavior

--- heritage/toh5.py
+++ heritage/toh5.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python
 
-import clean
+"""
+Convert the claims data to .h5 pandas HDF store.
 
-data = clean.loadRawData('./data')
-data['claim'] = clean.cleanClaim(data['claim'])
-clean.storeAsH5('HHP_release3.h5', data)
+python toh5.py [--safe]
+
+Use --safe flag if you are running out of memory
+"""
+
+import clean
+import util
+import sys
+
+def safe_read(file_name):
+    split_frame = pd.read_csv(file_name,iterator = True,chunksize = 1000)
+    return pd.concat(split_frame,ignore_index = True)
+
+def create_h5_file(datadir,filepath):
+    files = {"claim": "Claims.csv",
+            "drug":"DrugCount.csv",
+            "lab":"LabCount.csv",
+            "member": "Members.csv",
+            "dih"  : "DaysInHospital_Y2.csv",
+            "dih_y3" :"DaysInHospital_Y3.csv"
+            }
+    store = pd.HDFStore(filepath, 'w')
+    for category in files:
+        store[category] = safe_read(datadir + "/" + files[category])
+    store.close()
+
+if sys.argv[-1] == '--safe':
+    create_h5_file("./data","HHP_release3.h5")
+else:
+    data = clean.loadRawData('./data')
+    data['claim'] = clean.cleanClaim(data['claim'])
+    util.storeAsH5('./data/HHP_release3.h5', data)
